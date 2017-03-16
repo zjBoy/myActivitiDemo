be1619268dff626ef684df1bd57be535bf211a99
zjBoy <756839400@qq.com>
2017-03-16 17:26:21 +0800

    提交审批

--- myActivitiDemo/src/main/java/com/king/controller/activiti/ActivitiController.java
+++ myActivitiDemo/src/main/java/com/king/controller/activiti/ActivitiController.java
@@ -7,11 +7,14 @@ import java.util.Map;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,6 +34,8 @@ public class ActivitiController {
 	RepositoryService repositoryService;
 	@Autowired
 	RuntimeService runtimeService;
+	@Autowired
+	TaskService taskService;
 	/**
 	 * 发布流程
 	 * @return
@@ -49,19 +54,29 @@ public class ActivitiController {
 		deploymentBuilder.deploy();
 		return ErrorUtil.create("0000", "发布成功").toJSONString();
 	}
+	public void startProcess() {
+		runtimeService.startProcessInstanceByKey("leaveBill");
+	} 
 	 /**
-	  * 启动一个流程
+	  * 申请请假
 	  */
-	 @RequestMapping(value="/leaveBillApply",method=RequestMethod.POST)
 	 @ResponseBody
-	public String startProcess(@ModelAttribute("leaveBill") LeaveBill leaveBill) {
+	 @RequestMapping(value="/leaveBillApply",method=RequestMethod.POST)
+	 public String leaveBillApply(@RequestBody LeaveBill leaveBill) {
+		 //启动一个流程
+		 ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leaveBill");
+		 String executionId = processInstance.getProcessInstanceId();
+		 //提交请假
 		 Map<String, Object> map = new HashMap<String, Object>();
 		 map.put("userName", leaveBill.getUserName());
 		 map.put("days", leaveBill.getDays());
 		 map.put("reason", leaveBill.getReason());
-		runtimeService.startProcessInstanceByKey("leaveBill",map);
-		return ErrorUtil.create("0000", "启动流程成功").toJSONString();
-	} 
+		 Task task=taskService.createTaskQuery()
+		 			.executionId(executionId)
+		 			.singleResult();
+		 taskService.complete(task.getId(),map);
+		 return ErrorUtil.create("0000", "提交申请成功").toJSONString();
+	 } 
 	/**
 	 * 使用java代码初始化环境
 	 */
@@ -76,5 +91,12 @@ public class ActivitiController {
 	 @RequestMapping(value="/askForLeave")
 	 public String askForLeave() {
 		 return "askForLeave";
+	 }
+	 /**
+	  * 我的待审批任务
+	  */
+	 @RequestMapping(value="/myTaskList")
+	 public String myTaskList() {
+		 return "myTaskList";
 	 }
 }
